4cca97bf4da44e20b475ca5b007a1922ed5e4261
doublebyte1 <doublebyte@gmail.com>
2017-07-06 16:43:52 +0200

    - updated projects

--- docker/dashboard/docker-entry.sh
+++ docker/dashboard/docker-entry.sh
@@ -6,6 +6,21 @@ curl -X PUT http://elasticsearch:9200/indicators -d @/usr/local/tomcat/daobs/es/
 
 curl 'elasticsearch:9200/_cat/indices?v'
 
+echo -e "\e[91mLoad defaults for Kibana\e[96m"
+
+#cd /kibana/data;
+cd /usr/local/tomcat/daobs/dashboards/data/;
+
+elasticdump \
+  --input=http://elasticsearch:9200/.dashboards \
+  --output=index-dashboards-mapping.json \
+  --type=mapping
+
+elasticdump \
+  --input=http://elasticsearch:9200/.dashboards \
+  --output=index-dashboards.json
+
 echo -e "\e[96mStart tomcat\e[0m"
 
+cd $CATALINA_HOME
 catalina.sh run
--- docker/kibana/docker-entry.sh
+++ docker/kibana/docker-entry.sh
@@ -1,18 +1,16 @@
 #/bin/bash
 
-echo -e "\e[91mLoad defaults for Kibana\e[96m"
+#echo -e "\e[91mLoad defaults for Kibana\e[96m"
 
+/kibana/bin/kibana && \
+cd /kibana/data && \
 elasticdump \
   --input=http://elasticsearch:9200/.dashboards \
   --output=index-dashboards-mapping.json \
-  --type=mapping
-
+  --type=mapping && \
 elasticdump \
   --input=http://elasticsearch:9200/.dashboards \
   --output=index-dashboards.json
 
-curl 'elasticsearch:9200/_cat/indices?v'
+#curl 'elasticsearch:9200/_cat/indices?v'
 
-echo -e "\e[96mStart kibana\e[0m"
-
-/kibana/bin/kibana
