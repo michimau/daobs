0195ec22af5f70f22137a16349b0bc80c1932d83
michimau <mauro.michielon@eea.europa.eu>
2018-02-02 09:08:35 +0100

    kibana to use env variables

--- docker/kibana/docker-entry.sh
+++ docker/kibana/docker-entry.sh
@@ -5,3 +5,9 @@ cd /usr/share/kibana/
 /usr/share/kibana/bin/kibana-plugin remove x-pack
 
 /usr/share/kibana/bin/kibana
+
+cp /kibana.yml /usr/share/kibana/config/kibana.yml
+
+sed "s#BASEPATH#$BASEPATH#g" -i /usr/share/kibana/config/kibana.yml
+sed "s#KIBANAUSERNAME#$KIBANAUSERNAMEO#g" -i /usr/share/kibana/config/kibana.yml
+sed "s#KIBANAPASSWORD#$KIBANAPASSWORD#g" -i /usr/share/kibana/config/kibana.yml
